c7adcc1a4f17ba871573834018a42a82bdffc81d
euanleith <51672571+euanleith@users.noreply.github.com>
2020-02-26 13:54:14 +0000

    Added surface magnetism anchoring on click
    Added sample scene with an object which follows the user's view and sticks to surfaces, which anchors to that surface on click by the script anchorScript

---
+++ Spatial Mapping/anchorScript.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anchorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void anchor()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        this.gameObject.GetComponent<SolverHandler>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
