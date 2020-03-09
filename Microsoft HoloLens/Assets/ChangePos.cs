82dfa38516368d119288e50c080027537e4ac8d7
Darren Kitching <39224893+DarrenKitching@users.noreply.github.com>
2020-03-09 19:29:35 +0000

    Added script for piece position update

---
+++ Microsoft HoloLens/Assets/ChangePos.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static GameObject lastTouch = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Change() 
+    {
+        print("Hello");
+        if (lastTouch != null)
+        {
+            print("Coords of last piece are " + lastTouch.transform.position);
+            Vector3 temp = gameObject.transform.position;
+            temp.x += (float) 0.35;
+            temp.y += 1;
+            temp.z += (float) 4.2;
+            lastTouch.transform.position = temp;
+            print("New coords of last touch " + lastTouch.transform.position);
+        }
+        else
+        {
+            print("null");
+            lastTouch = gameObject;
+            print("Set last touch to " + lastTouch.name);
+        }
+    }
+}
